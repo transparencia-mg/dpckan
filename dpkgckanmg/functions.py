43ecf8cac361caa676feeccce3bba664bcb498e5
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-05-14 17:13:36 -0300

    [#1] - Modificando a função resources para módulo updateResources

--- dpkgckanmg/functions.py
+++ dpkgckanmg/functions.py
@@ -116,26 +116,6 @@ def criarArquivo(authorization,package_id,caminhoCompleto,separador):
               files = files)
     time.sleep(10)
 
-def resource(caminhoCompleto, id, authorizaton,separador=separador):
-    format = caminhoCompleto.split(separador)[-1]
-    #dataset_dictAtual = comparaDataSet(dataset_dict,resources)
-    #pprint.pprint("caminhoatualizado: " + caminhoCompleto +
-    #dataset_dictAtual["name"])
-    formato = format.split('.')[1]
-    files = {'upload': (caminhoCompleto.split(separador)[-1], open(caminhoCompleto, 'rb'), 'text/' + formato)}
-    pprint.pprint("Atualizacao de arquivo inicializada")
-    resultado = requests.post('http://homologa.cge.mg.gov.br/api/action/resource_update',
-              data={"id":id},
-              #data=dataset_dictAtual,
-              headers={"Authorization": authorizaton},
-              files = files)
-    if(resultado.iter_lines.__self__.status_code == 500):
-        pprint.pprint("Erro ao atualizar arquivo")
-    elif(resultado.iter_lines.__self__.status_code == 403):
-         pprint.pprint("Acesso negado. Verifique a autorizacao de acesso.")
-    else:
-        pprint.pprint("Atualizacao de arquivo finalizada")
-
 def lerDadosJson(diretorio,nomeArquivo):
     with open(diretorio) as json_file:
         data = json.load(json_file)
--- dpkgckanmg/updateResource.py
+++ dpkgckanmg/updateResource.py
@@ -6,8 +6,47 @@ import requests
 import json
 import collections
 import sys
-from functions import separador, buscaListaDadosAbertos,buscaDataSet,criarArquivo,importaDataSet,buscaPastaArquivos,removePastaArquivos,lerDadosJsonMapeado,buscaArquivos,atualizaMeta,resource
+from functions import separador, buscaListaDadosAbertos,buscaDataSet,criarArquivo,importaDataSet,buscaPastaArquivos,removePastaArquivos,lerDadosJsonMapeado,buscaArquivos,atualizaMeta
+
+def resource(caminhoCompleto, id, authorizaton,separador=separador):
+  """
+  Summary line.
+
+  Extended description of function.
+
+  Parameters
+  ----------
+  arg1 : int
+      Description of arg1
+  arg2 : str
+      Description of arg2
+
+  Returns
+  -------
+  int
+      Description of return value
+
+  """
+  format = caminhoCompleto.split(separador)[-1]
+  #dataset_dictAtual = comparaDataSet(dataset_dict,resources)
+  #pprint.pprint("caminhoatualizado: " + caminhoCompleto +
+  #dataset_dictAtual["name"])
+  formato = format.split('.')[1]
+  files = {'upload': (caminhoCompleto.split(separador)[-1], open(caminhoCompleto, 'rb'), 'text/' + formato)}
+  pprint.pprint("Atualizacao de arquivo inicializada")
+  resultado = requests.post('http://homologa.cge.mg.gov.br/api/action/resource_update',
+            data={"id":id},
+            #data=dataset_dictAtual,
+            headers={"Authorization": authorizaton},
+            files = files)
+  if(resultado.iter_lines.__self__.status_code == 500):
+      pprint.pprint("Erro ao atualizar arquivo")
+  elif(resultado.iter_lines.__self__.status_code == 403):
+       pprint.pprint("Acesso negado. Verifique a autorizacao de acesso.")
+  else:
+      pprint.pprint("Atualizacao de arquivo finalizada")
 
 #resource(local-onde-havia-caminho-maquina,'local-onde-havia-chave-acesso', 'local-onde-havia-chave-acesso', separador)
-if __name__ == '__main__':
-    resource(sys.argv[1], sys.argv[2], sys.argv[3], separador)
+# if __name__ == '__main__':
+#     resource(sys.argv[1], sys.argv[2], sys.argv[3], separador)
+# print(resource.__doc__)
