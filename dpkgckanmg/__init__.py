ed1fa534d5c6045ad815e534d03a3ba9cd7d51db
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-06-02 15:45:59 -0300

    [#15] facilitar a chamada das funções do pacote

--- dpkgckanmg/__init__.py
+++ dpkgckanmg/__init__.py
@@ -1,1 +1,4 @@
-
+from dpkgckanmg.createResource import criarArquivo2
+from dpkgckanmg.publish import publish
+from dpkgckanmg.updateDataSet import dataSet
+from dpkgckanmg.updateResource import resource
