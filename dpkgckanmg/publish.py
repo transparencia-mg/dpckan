1ad1190488ccb29fc33108286f852663ae52557e
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-06-04 15:03:42 -0300

    [#15] escolha do ambiente desejado durante utilização da função publish

--- dpkgckanmg/publish.py
+++ dpkgckanmg/publish.py
@@ -12,23 +12,27 @@ import codecs
 from dpkgckanmg.functions import separador,buscaListaDadosAbertos,buscaDataSet,criarArquivo,importaDataSet,buscaPastaArquivos,removePastaArquivos,lerDadosJsonMapeado,buscaArquivos,atualizaMeta,atualizaDicionario,lerCaminhoRelativo
 
 
-def publish(package_path,authorizaton):
+def publish(package_path, ckan_key, environment='homologa'):
   """
   Função responsável pela publicação de um conjunto de dados.
 
   Parameters
   ----------
   package_path : string
-      Caminho aonde o arquivo datapackage.json a ser publicado se encontra
-  autorization : string
-      ckan key do usuário no ambiente desejado
-
+    Caminho aonde o arquivo datapackage.json a ser publicado se encontra
+  ckan_key : string
+    ckan key do usuário no ambiente desejado
+    Sugerimos a criação de arquivo .env (não rastreado via .gitignore) para armazenamento das chaves de seus respectivos ambientes
+  environment: string
+    usuário de deverá escolher entre:
+      - homologa (para embiente de homologação - homologa.cge.mg.gov.br); e
+      - portal (para embiente de produção - dados.mg.gov.br).
+    Caso ambiente não seja informado publicação ocorrerá no ambiente de homologação
   Returns
   -------
   string
       Conjunto publicado no ambiente desejado e mensagem de sucesso:
       "Criacao de DataSet finalizada: <nome-do-conjunto>"
-
   """
   #try:
   #separador = os.path.sep
@@ -46,7 +50,7 @@ def publish(package_path,authorizaton):
          #nomePasta = arquivos[0]
          nameDataPackage = package_path.split(os_forward_slash_publish)[-1]
          pprint.pprint("Criacao de DataSet inicializada: " + nameDataPackage)
-         importaDataSet(authorizaton,"",package_path,"csv",privado,autor,type,tags,os_forward_slash_publish,"",comandoDelete,so)
+         importaDataSet(ckan_key,"",package_path,"csv",privado,autor,type,tags,os_forward_slash_publish,"",comandoDelete,so,environment)
          pprint.pprint("Criacao de DataSet finalizada: " + nameDataPackage)
          pprint.pprint("***********************************************************")
          #except Exception as e:
