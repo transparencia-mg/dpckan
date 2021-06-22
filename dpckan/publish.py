7c5886b1422e1ea33d3acc62d4a252ed84b055e1
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-06-22 15:15:05 -0300

    [#23] click commands in publish file/command and clean code

--- dpckan/publish.py
+++ dpckan/publish.py
@@ -10,36 +10,32 @@ import codecs
 import click
 from dpckan.functions import separador,buscaListaDadosAbertos,buscaDataSet,criarArquivo,importaDataSet,buscaPastaArquivos,removePastaArquivos,lerDadosJsonMapeado,buscaArquivos,atualizaMeta,atualizaDicionario,lerCaminhoRelativo
 
-@click.command(name='publish')
+@click.command()
 @click.option('--env',
               '-e',
-              required=True,
-              help='Ambiente aonde datasete será publicado (homologacao ou producao',
-              prompt='Qual ambiente deseja publicar/atualizar seu dataset? homologacao ou producao?')
+              help='''
+                Flag que completará as variáveis de ambiente CKAN_HOST e CKAN_KEY.
+                Exemplo --env HOMOLOGA para CKAN_HOST_HOMOLOGA e CKAN_KEY_HOMOLOGA
+              ''')
 def publish(package_path, ckan_key, environment='homologa'):
   """
   Função responsável pela publicação de um conjunto de dados no ambiente desejado.
 
+  Por padrão, função buscará Host e Key da instância CKAN para qual e deseja publicar/atualizar dataset
+  nas variáveis de ambiente CKAN_HOST e CKAN_KEY. Caso usuário deseja cadastrar mais de uma instância,
+  basta acrescentar uma "flag" ao final destes dois nomes, exemplo CKAN_HOST_HOMOLOGA e CKAN_KEY_HOMOLOGA
+
   Parameters
   ----------
-  package_path : string
-    Caminho aonde o arquivo datapackage.json a ser publicado se encontra
-  ckan_key : string
-    ckan key do usuário no ambiente desejado
-    Sugerimos a criação de arquivo .env (não rastreado via .gitignore) para armazenamento das chaves de seus respectivos ambientes
-  environment: string
-    usuário de deverá escolher entre:
-      - homologa (para embiente de homologação - homologa.cge.mg.gov.br); e
-      - portal (para embiente de produção - dados.mg.gov.br).
-    Caso ambiente não seja informado publicação ocorrerá no ambiente de homologação
+  env: string (não obrigatório)
+    Flag complementando as variáveis de ambiente padrão CKAN_HOST e CKAN_KEY
+
   Returns
   -------
   string
       Conjunto publicado no ambiente desejado e mensagem de sucesso:
       "Criacao de DataSet finalizada: <nome-do-conjunto>"
   """
-  #try:
-  #separador = os.path.sep
   os_forward_slash_publish = separador
   caminhoCompleto = package_path + os_forward_slash_publish + "datapackage" + '.json'
   if(os.path.isfile(caminhoCompleto)):
@@ -50,20 +46,9 @@ def publish(package_path, ckan_key, environment='homologa'):
       autor = 'Usuario teste'
       tags = [{"name": "my_tag"}, {"name": "my-other-tag"}]
       if ((caminhoRelativo.find('http')) or (len(os.listdir(caminhoRelativo)) > 0)):
-         #try:
-         #nomePasta = arquivos[0]
          nameDataPackage = package_path.split(os_forward_slash_publish)[-1]
          pprint.pprint("Criacao de DataSet inicializada: " + nameDataPackage)
          importaDataSet(ckan_key,"",package_path,"csv",privado,autor,type,tags,os_forward_slash_publish,"",comandoDelete,so,environment)
          pprint.pprint("Criacao de DataSet finalizada: " + nameDataPackage)
          pprint.pprint("***********************************************************")
-         #except Exception as e:
-             #print("Erro ao importar: " + arquivos[0] + " ERROR:" + e)
-  #except Exception as e:
-      #print(e)
 
-# if __name__ == '__main__':
-#   publish(sys.argv[1], sys.argv[2])
-#publish(local-onde-havia-caminho-maquina,'local-onde-havia-chave-acesso',separador)
-#atualizaDicionario(local-onde-havia-caminho-maquina,'local-onde-havia-chave-acesso','local-onde-havia-caminho-maquina','/')
-# print(publish.__doc__)
