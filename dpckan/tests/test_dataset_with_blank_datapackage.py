2c6ae1f7be9ba2bf1aa72aed3fe096bccd2db455
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-06-22 17:44:05 -0300

    [#26] tests folder migration to new dpckan folder structure - changes during clean folder process

---
+++ dpckan/tests/test_dataset_with_blank_datapackage.py
@@ -0,0 +1,41 @@
+import os
+from click.testing import CliRunner
+import unittest
+import dpckan_test
+from dpckan.publish import publish
+
+
+class TestDatasetWithBlankDatapackage(unittest.TestCase):
+  """
+    Testing error for lack of datapackage file inside dataset home directory
+  """
+  def test_homologa_env(self):
+    """
+      Testing error for lack of datapackage file inside dataset home directory (--env homologacao)
+    """
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=dpckan_test.get_file_path()):
+      dpckan_test.clone_online_repo(__file__)
+      result = runner.invoke(publish)
+      dpckan_test.clean_temp_folders()
+      self.assertNotEqual(result.exit_code, 0)
+      self.assertEqual(result.output,
+                        "----Arquivo datapackage.json com algum problema de sintaxe ou em branco----\n")
+
+
+  def test_production_env(self):
+    """
+      Testing error for lack of datapackage file inside dataset home directory (--env homologacao)
+    """
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=dpckan_test.get_file_path()):
+      dpckan_test.clone_online_repo(__file__)
+      result = runner.invoke(publish)
+      dpckan_test.clean_temp_folders()
+      self.assertNotEqual(result.exit_code, 0)
+      self.assertEqual(result.output,
+                        "----Arquivo datapackage.json com algum problema de sintaxe ou em branco----\n")
+
+if __name__ == '__main__':
+  unittest.main()
+  dpckan_test.clean_temp_folders()
---
+++ dpckan/tests/test_dataset_without_datapackage_file.py
@@ -0,0 +1,39 @@
+import os
+from click.testing import CliRunner
+import unittest
+import dpckan_test
+from dpckan.publish import publish
+
+
+class TestDatasetWithoutDatapackageFile(unittest.TestCase):
+  """
+    Testing error for lack of datapackage file inside dataset home directory
+  """
+  def test_homologa_env(self):
+    """
+      Testing error for lack of datapackage file inside dataset home directory (--env homologacao)
+    """
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=dpckan_test.get_file_path()):
+      dpckan_test.clone_online_repo(__file__)
+      result = runner.invoke(publish)
+      dpckan_test.clean_temp_folders()
+      self.assertNotEqual(result.exit_code, 0)
+      self.assertEqual(result.output,
+                        "----Arquivo datapackage.json não encontrado na raiz do dataset----\n")
+
+  def test_production_env(self):
+    """
+      Testing error for lack of datapackage file inside dataset home directory (--env homologacao)
+    """
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=dpckan_test.get_file_path()):
+      dpckan_test.clone_online_repo(__file__)
+      result = runner.invoke(publish)
+      dpckan_test.clean_temp_folders()
+      self.assertNotEqual(result.exit_code, 0)
+      self.assertEqual(result.output,
+                        "----Arquivo datapackage.json não encontrado na raiz do dataset----\n")
+
+if __name__ == '__main__':
+  unittest.main()
