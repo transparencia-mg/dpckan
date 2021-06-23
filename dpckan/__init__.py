b796cbcb74ccad66d44a51caddbb2403de92d159
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-06-23 18:22:32 -0300

    [#24] loading dotenv package to help environment variables grabbing process

--- dpckan/__init__.py
+++ dpckan/__init__.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=Path('.', '.env'))
 from dpckan.createResource import criarArquivo2
 from dpckan.publish import publish
 from dpckan.updateDataSet import dataSet
 from dpckan.updateResource import resource
+
