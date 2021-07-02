b4a7f60b2e584d67edc1502c16264f0bfe64bddd
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-07-02 10:12:30 -0300

    Withdraw duplicated function
    As pointed for @fjuniorr  is_datapackage_present function was duplicated in functions and validations modules.
    At first, is_datapackage_present was created in the functions module, but after a while, we realize that some validations must be realized before datasets publications/updates.
    To better code organization, the validations module was created and function migrated, but is_datapackage_present stays in the functions module wrongly.
    
    Resolves:  First observation of https://github.com/dados-mg/dpckan/pull/34#issuecomment-872613792

--- dpckan/functions.py
+++ dpckan/functions.py
@@ -508,45 +508,6 @@ def atualizaDicionario(url,datapackage,resource_id,resource,authorization,separa
   response_dict = json.loads(response.read())
   assert response_dict['success'] is True
 
-def is_datapackage_present(env):
-  """
-  Verifica a existência da chave "datapackage_resource_id" no arquivo datapackage.json para o ambiente desejado (homologação ou produção)
-  Existência da chave "datapackage_resource_id" significa que pacote já foi publicado no ambiente desejado
-  Arquivo datapackage.json deve estar na raiz do dataset
-  Trata erro para:
-   - datapackage.json inexistente
-   - algum erro de sintaxe no arquivo datapackage.json, ou até mesmo arquivo existente mas em branco
-
-  Parameters
-  ----------
-    env
-    homologacao: homologa.cge.mg.gov.br/
-    producao: dados.mg.gov.br/
-
-
-  Returns
-  -------
-  string
-    valor correspondente a chave "datapackage_resource_id" do ambiente solicitado, quando a mesma existir
-  None
-    quando a chave não existir
-  """
-  try:
-    with codecs.open('datapackage.json','r', 'utf-8-sig') as json_file:
-      datapackage_keys = json.load(json_file)
-      if f"datapackage_resource_id_{env}" in datapackage_keys.keys():
-        click.echo("----Iniciando atualização do Dataset----")
-        return datapackage_keys[f"datapackage_resource_id_{env}"]
-      else:
-        click.echo("----Iniciando Publicação do Dataset----")
-        return None
-  except FileNotFoundError:
-    click.echo("----Arquivo datapackage.json não encontrado na raiz do dataset----")
-    sys.exit(1)
-  except json.decoder.JSONDecodeError:
-    click.echo("----Arquivo datapackage.json com algum problema de sintaxe ou em branco----")
-    sys.exit(1)
-
 def delete_dataset(host, key, dataset_name):
   demo = RemoteCKAN(host, apikey=key)
   demo.action.package_delete(id=dataset_name)
