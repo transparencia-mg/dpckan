1bcfd6842f78916d5163683d67bacd4b0b92f962
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-07-14 20:29:59 -0300

    Add dpckan dataset and resource subcommands
    Resolves #73

--- dpckan/update_resource.py
+++ dpckan/update_resource.py
@@ -12,11 +12,11 @@ from dpckan.functions import (load_complete_datapackage,
 @click.option('--ckan-key', '-k', envvar='CKAN_KEY', required=True,
               help="Ckan key autorizando o usuário a realizar publicações/atualizações em datasets")
 @click.option('--datapackage', '-dp', required=True, default='datapackage.json')
-@click.option('--resource-name', '-n', required=True,
-              help="Nome do recurso a ser incluído. Chave 'name' do recurso dentro do arquivo datapackage.json")
 @click.option('--resource-id', '-id', required=True,
               help="Id do recurso a ser incluído.")
-def update_resource(ckan_host, ckan_key, datapackage, resource_name, resource_id):
+@click.argument('resource_name', required=True)
+
+def update_resource(ckan_host, ckan_key, datapackage, resource_id, resource_name):
   """
   Summary line.
 
