8753b9be905e1f006d3abcc4ebccffd0b9cccbbf
Gabriel Dornas <gabrielbdornas@gmail.com>
2021-09-04 08:03:35 -0300

    Using create resource inside python file See #83

--- dpckan/update_resource.py
+++ dpckan/update_resource.py
@@ -6,16 +6,6 @@ from dpckan.functions import (load_complete_datapackage,
                               resource_update_datastore_metadata,
                               dataset_update)
 
-@click.command()
-@click.option('--ckan-host', '-H', envvar='CKAN_HOST', required=True,
-              help="Ckan host, exemplo: http://dados.mg.gov.br ou https://dados.mg.gov.br")  # -H para respeitar convenção de -h ser help
-@click.option('--ckan-key', '-k', envvar='CKAN_KEY', required=True,
-              help="Ckan key autorizando o usuário a realizar publicações/atualizações em datasets")
-@click.option('--datapackage', '-dp', required=True, default='datapackage.json')
-@click.option('--resource-id', '-id', required=True,
-              help="Id do recurso a ser incluído.")
-@click.argument('resource_name', required=True)
-
 def update_resource(ckan_host, ckan_key, datapackage, resource_id, resource_name):
   """
   Summary line.
@@ -47,3 +37,34 @@ def update_resource(ckan_host, ckan_key, datapackage, resource_id, resource_name
   resource_update_datastore_metadata(ckan_instance,
                             resource_id,
                             package.get_resource(resource_name))
+
+
+@click.command()
+@click.option('--ckan-host', '-H', envvar='CKAN_HOST', required=True,
+              help="Ckan host, exemplo: http://dados.mg.gov.br ou https://dados.mg.gov.br")  # -H para respeitar convenção de -h ser help
+@click.option('--ckan-key', '-k', envvar='CKAN_KEY', required=True,
+              help="Ckan key autorizando o usuário a realizar publicações/atualizações em datasets")
+@click.option('--datapackage', '-dp', required=True, default='datapackage.json')
+@click.option('--resource-id', '-id', required=True,
+              help="Id do recurso a ser incluído.")
+@click.option('--resource-name', '-rn', required=True)
+def update_resource_cli(ckan_host, ckan_key, datapackage, resource_id, resource_name):
+  """
+  Summary line.
+
+  Extended description of function.
+
+  Parameters
+  ----------
+  arg1 : int
+      Description of arg1
+  arg2 : str
+      Description of arg2
+
+  Returns
+  -------
+  int
+      Description of return value
+
+  """
+  update_resource(ckan_host, ckan_key, datapackage, resource_id, resource_name)
