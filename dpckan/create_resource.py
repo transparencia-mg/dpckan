a1592e48ac22e4b9f586a733e96be6e618f2e467
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-01-27 16:29:08 -0300

    Improve docstrings resource create
    Use new file with common docstrings definition in resource create file
    Apply DRY concepts
    See #142

--- dpckan/create_resource.py
+++ dpckan/create_resource.py
@@ -1,6 +1,12 @@
 import click
 from ckanapi import RemoteCKAN
 from dpckan.validations import run_resource_validations
+from dpckan.docstrings_common_definitions import (host_flag_,
+                                                 key_flag_,
+                                                 datapackage_flag_,
+                                                 resource_id_flag_,
+                                                 resource_name_flag_,
+                                                 stop_flag_,)
 from dpckan.functions import (load_complete_datapackage, 
                               update_datapackage_json_resource, 
                               resource_create,
@@ -10,36 +16,6 @@ from dpckan.functions import (load_complete_datapackage,
                               get_ckan_datapackage_resource_id,)
 
 def create_resource(ckan_host, ckan_key, datapackage, resource_name, stop):
-  """
-  Função responsável pela publicação de um recurso em conjunto de dados já existente na instância CKAN desejada.
-
-  Parâmetros:
-
-  ----------
-
-  ckan_host: string
-
-    host ou ambiente da instância CKAN para a qual se deseja publicar conjunto de dados.
-    Exemplo: https://demo.ckan.org/
-
-  ckan_key: string
-
-    Chave CKAN do usuário e ambiente para a qual se deseja publicar conjunto de dados.
-
-  datapackage: string
-
-    Caminho local para arquivo datapackage.json.
-
-  resource_name: string
-
-    Nome do recurso que será criado. Propriedade `name` do resource dentro do arquivo datapackage.json.
-
-  Retorna
-
-  -------
-
-  Recurso criado em um conjunto de dados previamente publicado no ambiente desejado.
-  """
   package = load_complete_datapackage(datapackage)
   ckan_instance = RemoteCKAN(ckan_host, apikey = ckan_key)
   run_resource_validations(ckan_instance, package, stop)
@@ -59,46 +35,17 @@ def create_resource(ckan_host, ckan_key, datapackage, resource_name, stop):
 
 @click.command()
 @click.option('--ckan-host', '-H', envvar='CKAN_HOST', required=True,
-              help="Ckan host, exemplo: https://demo.ckan.org/")  # -H para respeitar convenção de -h ser help
+              help=host_flag_) # -H to respect convention of -h in help option
 @click.option('--ckan-key', '-k', envvar='CKAN_KEY', required=True,
-              help="Ckan key autorizando o usuário a realizar publicações/atualizações em datasets")
+              help=key_flag_)
 @click.option('--datapackage', '-dp', required=True, default='datapackage.json',
-              help="Caminho para arquivo datapackage.json")
-@click.option('--resource-name', '-rn', required=True)
-@click.option('--stop', '-s', is_flag=True, help="Parar execução caso validação frictionless retorne algum erro")
+              help=datapackage_flag_)
+@click.option('--resource-name', '-rn', required=True,
+              help=resource_name_flag_)
+@click.option('--stop', '-s', is_flag=True, help=stop_flag_)
 def create_resource_cli(ckan_host, ckan_key, datapackage, resource_name, stop):
   """
-  Função CLI responsável pela publicação de um recurso em conjunto de dados já existente na instância CKAN desejada.
-
-  Por padrão, função buscará host e key da instância CKAN nas variáveis de ambiente CKAN_HOST e CKAN_KEY cadastradas na máquina ou
-  em arquivo .env na raiz do dataset.
-
-  Parâmetros:
-
-  ----------
-
-  ckan_host: string (não obrigatório caso variável CKAN_HOST esteja cadastrada na máquina ou em arquivo .env)
-
-    host ou ambiente da instância CKAN para a qual se deseja publicar conjunto de dados.
-    Exemplo: https://demo.ckan.org/
-
-  ckan_key: string (não obrigatório caso variável CKAN_KEY esteja cadastrada na máquina ou em arquivo .env)
-
-    Chave CKAN do usuário e ambiente para a qual se deseja publicar conjunto de dados.
-
-  datapackage: string (não obrigatório caso comando seja executado no mesmo diretório do arquivo datapackage.json)
-
-    Caminho local para arquivo datapackage.json.
-
-  resource_name: string
-
-    Nome do recurso que será criado. Propriedade `name` do resource dentro do arquivo datapackage.json.
-
-  Retorna:
-
-  -------
-
-  Recurso criado em um conjunto de dados previamente publicado no ambiente desejado.
+  Create a resource in a dataset published in ckan.
   """
   create_resource(ckan_host, ckan_key, datapackage, resource_name, stop)
 
