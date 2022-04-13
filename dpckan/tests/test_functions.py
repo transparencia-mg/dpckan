70df3643d566169c52607658281bfe359910265e
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-04-13 15:45:52 -0300

    Add uuid test

--- dpckan/tests/test_functions.py
+++ dpckan/tests/test_functions.py
@@ -1,6 +1,10 @@
 from frictionless import Package
-from dpckan.functions import (load_complete_datapackage,
-                              get_ckan_dataset_resources_names,)
+from dpckan.functions import (
+                              load_complete_datapackage,
+                              get_ckan_dataset_resources_names,
+                              is_uuid,
+                              )
+import pytest
 
 source = './dpckan/tests/dataset_test_1/datapackage.json'
 
@@ -18,3 +22,14 @@ def test_get_ckan_dataset_resources_names_list():
                                 'datapackage.json': 'd19de465-16d1-445a-934f-d11f29cadbee',}
   ckan_dataset_resources_names = get_ckan_dataset_resources_names(ckan_dataset_resources_ids)
   assert isinstance(ckan_dataset_resources_names, list) == True
+
+@pytest.mark.parametrize(
+                          "_input,expect",
+                          [
+                           ('078c9abe-d53d-4e3c-8165-fabbe2d20e0c', True),
+                           ('078c9abe-d53d-4e3c-8165-fabbe2d20e0', False),
+                           ('078c9abe-d53d-4e3c-8165-fabbe2d20e0d', True),
+                           ('ç78c9abe-d53d-4e3c-8165-fabbe2d20e0d', False),
+                          ])
+def test_is_uuid(_input,expect):
+  assert is_uuid(_input) == expect
