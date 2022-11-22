1c874c7a087ac260ea9882e4bb7251a7b20552a3
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-11-22 12:34:48 -0300

    Iprove log messages
    See #193

--- dpckan/update_dataset.py
+++ dpckan/update_dataset.py
@@ -1,16 +1,25 @@
 import click
 from ckanapi import RemoteCKAN
+from dpckan.validations import run_dataset_validations
 from dpckan.functions import (load_complete_datapackage, 
                               dataset_update, 
-                              update_datapackage_json_resource,)
+                              update_datapackage_json_resource,
+                              dataset_path,
+                              )
                               
 def update(ckan_host, ckan_key, datapackage, datastore, exit_code):
-  package = load_complete_datapackage(datapackage)
-
+  local_datapackage = load_complete_datapackage(datapackage)
+  dataset = dataset_path(ckan_host, local_datapackage)
+  click.echo(f"Updating dataset {dataset}")
   ckan_instance = RemoteCKAN(ckan_host, apikey = ckan_key)
-
-  # update_datapackage_json_resource(ckan_instance, package)
-  dataset_update(ckan_instance, package, datastore, exit_code)
+  try:
+    # run_resource_validations(ckan_instance, local_package)
+    dataset_update(ckan_instance, local_datapackage, datastore, exit_code)
+    click.echo(f"Dataset {dataset} updated")
+  except Exception:
+    click.echo(f"Error during {dataset} updating")
+    if exit_code == True:
+      sys.exit(1)
 
 @click.command(name='update')
 @click.pass_context
